97a54c28790c44015a66ad5d349d24171c4420e9
yrmadev <yrmadev@outlook.com>
2020-09-11 10:32:29 -0300

    se agrego rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:PortfolioComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'item', component:ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true} )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
