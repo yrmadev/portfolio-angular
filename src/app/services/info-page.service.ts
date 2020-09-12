a61289b1c3a8815483d05d4f1eaa5b72a5d38360
yrmadev <yrmadev@outlook.com>
2020-09-12 12:06:31 -0300

    show data page about from firebase

--- src/app/services/info-page.service.ts
+++ src/app/services/info-page.service.ts
@@ -6,19 +6,36 @@ import { InfoPage } from '../interfaces/info-page.interface';
   providedIn: 'root'
 })
 export class InfoPageService {
+  
   info: InfoPage= {};
   load = false;
+  teamPage: any[] = [];
+
   constructor(private http: HttpClient) {
     console.log("init service");
+    this.getInfo();
+    this.getTeam();
 
+   }
+
+   private getInfo(){
     //read json file
     this.http.get('assets/data/data-page.json')
         .subscribe((resp: InfoPage) =>{
           this.load = true;
           this.info = resp;
-          console.log(resp);
-          console.log(resp['email']);
+          //console.log(resp);
+          //console.log(resp['email']);
         })
+   }
+   
+   private getTeam(){
+    //read json file from firebase
+    this.http.get('https://angular-html-56a07.firebaseio.com/team.json')
+    .subscribe((resp:any) =>{
+      this.teamPage = resp;
+      console.log(resp);
+    })
 
    }
 }
